bcc2529e49ecc66978d5c3000013b20431c9ccd0
DESKTOP-OGDKKJ9\USER <PhamQuang@activants.com>
2024-10-09 10:13:16 +0800

    Implement Bayes Decision Rule and add gitignore

--- train_BayesDecision.m
+++ train_BayesDecision.m
@@ -1,7 +1,30 @@
-% https://www.mathworks.com/help/stats/discriminant-analysis.html
-% https://www.mathworks.com/help/stats/improving-discriminant-analysis-models.html
-BayesDecision_Mdl = fitcdiscr(X, label, "ClassNames", {'-1', '1'});
+% size(X, 2) => (X, dim=2) => no of cols = 5]
+numFeatures = size(X, 2);
 
-resubLoss(BayesDecision_Mdl)
+Mu = zeros(numClasses, numFeatures); 
+Sigma = zeros(numClasses, numFeatures);
+Prior = zeros(numClasses, 1); % Prior probabilities
 
+for i = 1:numClasses
+    idx = find(y==classes(i));
+    Mu(i, :) = mean(X(idx,:)); % mean of features for class i
+    Sigma(i, :) = std(X(idx,:)); % Covariance matrix for class i 
+    Prior(i) = sum(idx) / length(y); % Prior probability or class i
+end
 
+function predictedClass = bayesDecisionRule(X_new, Mu, Sigma, Prior, numClasses)
+    Posteriors = zeros(numClasses, length(X_new));
+    likelihood =  normpdf(X_new, Mu, Sigma);
+
+    for i = 1:numClasses
+        Posteriors(i, :) = Prior(i) * likelihood(i, :); % Compute posterior P(Y|X)
+    end
+    % [Matrix, Index]
+    [~, predictedClass] = max(Posteriors); % Classify as the class with max posterior probability
+end
+
+% Test classification on a new data point
+newDataPoint = [5.0, 3.5, 1.5, 0.2, 0.5]; % Example data point
+predictedClassIndex = bayesDecisionRule(newDataPoint, Mu, Sigma, Prior, numClasses);
+
+disp(['Predicted class: ', num2str(classes(predictedClassIndex(:,1))) ]);
